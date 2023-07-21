e701fdfaa5274c432f408e0798cc2a88b26e9849
duy-pham <duypham250796@gmail.com>
2023-07-21 15:16:25 +0700

    push code

---
+++ app.js
@@ -0,0 +1,57 @@
+var button = document.getElementById("btn")
+
+
+// Sử dụng màu rgb
+// var changeBackGround = () => {
+//     var green = Math.floor(Math.random() * 256);
+//     var red = Math.floor(Math.random() * 256);
+//     var blue = Math.floor(Math.random() * 256);
+
+//     var rgb = "rgb(" + red + ", " + green + ", " + blue + ")";
+//     console.log(rgb);
+
+
+//     var color = document.getElementsByClassName("color");
+//     color[0].innerHTML = rgb;
+
+//     var main = document.getElementsByTagName("main");
+//     main[0].style.backgroundColor = rgb;
+// }
+
+// Sử dụng tên màu 
+var changeBackGround = () => {
+    var colorNames = [
+        "aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque",
+        "black", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue",
+        "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan",
+        "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkkhaki", "darkmagenta",
+        "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen",
+        "darkslateblue", "darkslategray", "darkturquoise", "darkviolet", "deeppink", "deepskyblue",
+        "dimgray", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro",
+        "ghostwhite", "gold", "goldenrod", "gray", "green", "greenyellow", "honeydew", "hotpink",
+        "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen",
+        "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray",
+        "lightgreen", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray",
+        "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon",
+        "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen",
+        "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue",
+        "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab",
+        "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise",
+        "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue",
+        "purple", "rebeccapurple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon",
+        "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray",
+        "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet",
+        "wheat", "white", "whitesmoke", "yellow", "yellowgreen"
+    ];
+
+    var index = Math.floor(Math.random() * colorNames.length);
+    var color = colorNames[index];
+    var span = document.getElementsByClassName("color");
+    span[0].innerHTML = color;
+    var background = document.getElementsByTagName("main");
+    background[0].style.backgroundColor = color;
+    console.log(colorNames[index]);
+
+
+}
+button.addEventListener("click", changeBackGround);
